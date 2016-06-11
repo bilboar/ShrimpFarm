6eb65f4ac196c6046f70fc21c3568da33ccc7e11
Fred Bumbarger <fbumbarger@gmail.com>
2016-06-10 23:52:54 -0700

    Initial ShrimpFarm MVC5 commit!

---
+++ MyShrimpFarm/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShrimpFarm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "The <strong>All-Spark</strong> of shrimping applications.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "How to reach us.";
+
+            return View();
+        }
+    }
+}
---
+++ MyShrimpFarm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShrimpFarm.Startup))]
+namespace MyShrimpFarm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
